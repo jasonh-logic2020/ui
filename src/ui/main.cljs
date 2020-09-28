aefc8c08182a5338e23f08900b99fc7ed5ce9aa3
jasonh <jasonh@logic2020.com>
2020-09-28 15:17:11 -0700

    Minimal locations.cljs which only edits

--- src/ui/main.cljs
+++ src/ui/main.cljs
@@ -5,14 +5,6 @@
    [day8.re-frame.tracing :refer-macros [fn-traced]]
    ["react" :as react]
    ["@material-ui/core" :refer [ThemeProvider createMuiTheme]]
-   ["@material-ui/core" :refer [ThemeProvider createMuiTheme
-                                Badge Container Grid Paper FormControl
-                                Table TableCell TableRow MenuItem
-                                TableHead TableBody Popover InputLabel
-                                Link Tooltip Icon IconButton Select
-                                Button TextField withStyles Avatar Typography
-                                Dialog DialogTitle DialogActions Paper
-                                DialogContent DialogContentText] :as mui]
    [goog.object :as gobj]
    [ui.views.locations :as locations]
    [ui.routes :as routes]
@@ -24,75 +16,6 @@
 (def debug?
   ^boolean goog.DEBUG)
 
-(def drawer-width 240)
-
-(defn styles [^js/Mui.Theme theme]
-  (clj->js
-   {:root {:display "flex"}
-    :toolbar {:paddingRight 24} ; keep right padding when drawer closed
-    :toolbarIcon (merge {:display "flex"
-                         :alignItems "center"
-                         :justifyContent "flex-end"
-                         :padding "0 8px"}
-                        (js->clj (.. theme -mixins -toolbar)))
-    :appBar {:zIndex (+ (.. theme -zIndex -drawer) 1)
-             :transition
-             (.. theme -transitions
-                 (create #js ["width" "margin"]
-                         #js
-                         {:easing
-                          (.. theme -transitions -easing -sharp)
-                          :duration
-                          (.. theme -transitions -duration -leavingScreen)}))}
-    :appBarShift {:marginLeft drawer-width
-                  :width (str "calc(100% - " drawer-width "px)")
-                  :transition
-                  (.. theme -transitions
-                      (create #js ["width" "margin"]
-                              #js
-                              {:easing
-                               (.. theme -transitions -easing -sharp)
-                               :duration
-                               (.. theme -transitions -duration
-                                   -enteringScreen)}))}
-    :menuButton {:marginRight 36}
-    :menuButtonHidden {:display "none"}
-    :title {:flexGrow 1}
-    :drawerPaper {:position "relative"
-                  :whiteSpace "nowrap"
-                  :width drawer-width
-                  :transition
-                  (.. theme -transitions
-                      (create "width"
-                              #js
-                              {:easing (.. theme -transitions -easing -sharp)
-                               :duration
-                               (.. theme -transitions -duration
-                                   -enteringScreen)}))}
-    :drawerPaperClose {:overflowX "hidden"
-                       :transition
-                       (.. theme -transitions
-                           (create "width"
-                                   #js
-                                   {:easing (.. theme -transitions -easing
-                                                -sharp)
-                                    :duration (.. theme -transitions -duration
-                                                  -leavingScreen)}))
-                       :width (.spacing theme 7)
-                       (.breakpoints.up theme "sm") {:width (.spacing theme 9)}}
-    :appBarSpacer (.. theme -mixins -toolbar)
-    :content {:flexGrow 1
-              :height "100vh"
-              :overflow "auto"}
-    :container {:paddingTop (.spacing theme 4)
-                :paddingBottom (.spacing theme 4)}
-    :paper {:padding (.spacing theme 4)
-            :display "flex"
-            :overflow "auto"
-            :flexDirection "column"}
-    :fixedHeight {:height 240}}))
-
-(def with-styles (withStyles styles))
 
 (defn dev-setup []
   (when debug?
@@ -102,15 +25,10 @@
 ;;; DB
 
 (def default-db
-  {:dark-theme? false
-   :modal-data {:name "should never appear"}
-   :drawer/open? true
+  {:modal-data {:name "should never appear"}
    :add-open? false
    :edit-open? false
    :delete-open? false
-   :user {:id "204201e3-d5c4-40c3-a672-22b886e81ed2"
-          :name "Admin Role"
-          :admin true}
    :locations [{:name "Widget One"
                 :url "https://sample.org/1"
                 :policy "Low"
@@ -144,40 +62,18 @@
  (fn-traced [_ _]
             default-db))
 
-(rf/reg-event-db
- :toggle-dark-theme
- (fn-traced [db _]
-   (update db :dark-theme? not)))
 
 ;;; Subs
 
-(rf/reg-sub
- :db
- (fn [db]
-   db))
-
-(rf/reg-sub
- :dark-theme?
- (fn [db]
-   (:dark-theme? db)))
-
-(rf/reg-sub
- :errors
- (fn [db]
-   (:errors db)))
-
 ;;; Styles
-
-(defn custom-theme [dark-theme?]
-  (createMuiTheme (clj->js {:palette {:type (if dark-theme? "dark" "light")}
-                            :status {:danger "red"}})))
 
 ;;; Views
 
 (defn main-shell [{:keys [router classes]}]
-  (let [current-route @(rf/subscribe [:current-route])
-        dark-theme? @(rf/subscribe [:dark-theme?])]
-    [:> ThemeProvider {:theme (custom-theme dark-theme?)}
+  (let [current-route @(rf/subscribe [:current-route])]
+    [:> ThemeProvider {:theme (createMuiTheme
+                               (clj->js {:palette {:type "light"}
+                                         :status {:danger "red"}}))}
      [wrapper/page {:router router :current-route current-route}]]))
 
 ;;; Core
--- src/ui/views/locations.cljs
+++ src/ui/views/locations.cljs
@@ -22,25 +22,6 @@
                                 Dialog DialogTitle DialogActions Paper
                                 DialogContent DialogContentText] :as mui]))
 
-(defn styles [^js/Mui.Theme theme]
-  (clj->js
-   {:popover {:pointerEvents "none"}
-    :hoverable {"&.hover" {:backgroundColor "red !important"}}
-    :paper {:marginTop (.spacing theme 1)
-            :display "flex"
-            :flexDirection "column"
-            :alignItems "left"}
-    :hide {:display "none"}
-    :good {:style {:color "Green"}}
-    :bad {:color "Red"}
-    :avatar {:margin (.spacing theme 1)
-             :backgroundColor (.. theme -palette -secondary -main)}
-    :form {:marginTop (.spacing theme 1)}
-    :button {:margin (.spacing theme 1)}
-    :submit {:margin (.spacing theme 8)}}))
-
-(def with-styles (mui/withStyles styles))
-
 
 ;;; Subs
 
@@ -55,80 +36,13 @@
             (:locations db)))
 
 (rf/reg-sub
- :add-open?
- (fn-traced [db _]
-            (:add-open? db)))
-
-(rf/reg-sub
  :edit-open?
  (fn-traced [db _]
             (:edit-open? db)))
 
-(rf/reg-sub
- :delete-open?
- (fn-traced [db _]
-            (:delete-open? db)))
 
 ;;; Events
 
-;; --
-
-(rf/reg-event-db
- ::open-delete-dialog
- (fn-traced [db _]
-            (assoc db :delete-open? true)))
-
-(rf/reg-event-db
- ::close-delete-dialog
- (fn-traced [db _]
-            (assoc db :delete-open? false)))
-
-;; --
-
-(rf/reg-event-fx
- ::delete-location
- (fn-traced [{db :db} the-data]
-            (prn "delete-location" the-data)
-            {:db (assoc db :modal-data (last the-data))
-             :dispatch [::open-delete-dialog]}))
-
-(rf/reg-event-fx
- ::delete-submit-handler
- (fn-traced [{db :db} [ig {:keys [values path] :as props}]]
-            (prn "delete-submit-handler props" props)
-            (prn "delete-submit-handler" (:values props))
-            {:db (fork/set-submitting db :form true)
-             :dispatch [::delete-local props]}))
-
-(rf/reg-event-fx
- ::delete-local
- (fn-traced [{db :db} [_ {:keys [values reset dirty path] :as props}]]
-            (prn "delete-local props" props)
-            (prn "delete-local" values)
-            {:db (assoc db :locations
-                        (filter #(not= % {:name (get values "name")
-                                          :url (get values "url")
-                                          :policy (get values "policy")
-                                          :status (get values "status")
-                                          :ts (get values "ts")
-                                          :id (get values "id")})
-                                (:locations db)))
-             :dispatch [::delete-resolved-form props]}))
-
-(rf/reg-event-fx
- ::delete-resolved-form
- (fn-traced [{db :db} [_ {:keys [values path ] :as props}]]
-            (prn "resolved-form" values)
-            {:db (fork/set-submitting db path false)
-             :dispatch [::close-delete-dialog]}))
-
-(rf/reg-event-fx
- :cancel-delete
- (fn-traced [{db :db} [ig {:keys [values path]}]]
-            {:db (fork/set-submitting db :form false)
-             :dispatch [::close-delete-dialog]}))
-
-;; --
 
 (rf/reg-event-db
  ::open-edit-dialog
@@ -165,15 +79,6 @@
             {:db (fork/set-submitting db :form true)
              :dispatch [::edit-local props]}))
 
-#_(rf/reg-event-fx
-   ::edit-submit-handler
-   (fn-traced [{db :db} [_ {:keys [values reset dirty path] :as props}]]
-              (prn "edit-submit-handler props" props)
-              (prn "edit-submit-handler" (:values props))
-              (reset)
-              {:db (fork/set-submitting db path true)
-               :dispatch [::edit-local props]}))
-
 (rf/reg-event-fx
  ::edit-local
  (fn-traced [{:keys [db]} [_ {:keys [values] :as props}]]
@@ -201,93 +106,7 @@
 
 ;; --
 
-#_(rf/reg-event-fx
-   :add-start
-   (fn-traced [{db :db} [_ {:keys [values reset dirty path] :as props}]]
-              (prn "add-submit-handler props" props)
-              (prn "add-submit-handler" (:values props))
-              (reset)
-              {:db (fork/set-submitting db path true)
-               :dispatch [::add-local props]}))
-
-(rf/reg-event-db
- ::open-add-dialog
- (fn-traced [db _]
-            (assoc db :add-open? true)))
-
-(rf/reg-event-db
- ::close-add-dialog
- (fn-traced [db _]
-            (assoc db :add-open? false)))
-
-;; --
-
-(rf/reg-event-fx
- ::delete-success
- (fn-traced [{db :db} [_ result]]
-            {:db (assoc db :beagle-location result)}))
-
-(rf/reg-event-fx
- ::delete-failure
- (fn-traced [{db :db} [_ result path]]
-            {:db (-> db
-                     (fork/set-submitting path false)
-                     (fork/set-server-message path "'Add location' failed"))}))
-
-;; --
-
-
-;; --
-
-(rf/reg-event-fx
- :add-loction-success
- (fn-traced [{db :db} [_ result]]
-            {:db (assoc db :beagle-location result)}))
-
-(rf/reg-event-fx
- :add-location-failure
- (fn-traced [{db :db} [_ result path]]
-            {:db (-> db
-                     (fork/set-submitting path false)
-                     (fork/set-server-message path "'Add location' failed"))}))
-
-(rf/reg-event-fx
- :cancel-add
- (fn-traced [{db :db} [ig {:keys [values path]}]]
-            {:db (fork/set-submitting db :form false)
-             :dispatch [::close-add-dialog]}))
-
-(rf/reg-event-fx
- ::add-submit-handler
- (fn-traced [{db :db} [ig {:keys [values path] :as props}]]
-            (prn "add-submit-handler props" props)
-            (prn "add-submit-handler" (:values props))
-            {:db (fork/set-submitting db :form true)
-             :dispatch [::add-local props]}))
-
-(rf/reg-event-fx
- ::add-local
- (fn-traced [{:keys [db]} [_ {:keys [values] :as props}]]
-            (prn "add-local props" props)
-            (prn "add-local" values)
-            {:db (assoc db :locations
-                        (conj (:locations db) {:name (get values "name")
-                                               :url (get values "url")
-                                               :policy (get values "policy")
-                                               :status [:success]
-                                               :ts (t/now)
-                                               :id (random-uuid)}))
-             :dispatch [::add-resolved-form props]}))
-
-(rf/reg-event-fx
- ::add-resolved-form
- (fn-traced [{db :db} [_ {:keys [values path]}]]
-            (prn "resolved-form" values)
-            {:db (fork/set-submitting db path false)
-             :dispatch [::close-add-dialog]}))
-
-
-    ;; Components
+;; Components
 
 (defn drawer-icon []
   [:> icons/LocationOn])
@@ -310,187 +129,6 @@
                         (vlad/chain
                          (vlad/present)
                          (vlad/one-of #{"Low" "High"})))))
-
-(defn delete-dialog-form [{:keys [^js classes]}
-                          {:keys [values
-                                  props
-                                  state
-                                  form-id
-                                  errors
-                                  set-values
-                                  handle-change
-                                  handle-blur
-                                  handle-submit
-                                  reset
-                                  submitting?
-                                  touched
-                                  on-submit-response]}]
-  (let [{:keys [is-open?]} props]
-    [:form
-     {:id        form-id
-      :on-submit handle-submit}
-     [:> mui/Dialog {:fullWidth       true
-                     :open            @is-open?
-                     :on-close        #(rf/dispatch
-                                        [:add-location/close-dialog])
-                     :aria-labelledby :add-location-title}
-      [:> mui/DialogTitle {:id :add-location-title} "Confirm"]
-      [:> mui/DialogContent
-       [:> mui/DialogContentText (str "Are you sure you want to delete "
-                                      (values "name"))]
-       #_[:div [:pre (with-out-str
-                       (cljs.pprint/pprint touched)
-                       (cljs.pprint/pprint values)
-                       (cljs.pprint/pprint (or errors
-                                               submitting?)))]]]
-      [:> DialogActions
-       [:> Button {:label    "No"
-                   :on-blur   handle-blur
-                   :color    "primary"
-                   :on-click (fn [e]
-                               (reset)
-                               (rf/dispatch [:cancel-delete]))}
-        "No"]
-       [:> Button {:type     "submit"
-                   :on-blur   handle-blur
-                   :class    (.-submit classes)
-                   :color    "primary"
-                   :disabled (or errors submitting?)
-                   :on-click handle-submit
-                   :variant  "contained"
-                   :label    "Yes"} "Yes"]]]]))
-
-(defn delete-dialog [props]
-  [fork/form {:form-id         "delete"
-              :initial-values  (w/stringify-keys @(rf/subscribe
-                                                   [:modal-data]))
-              :validation #(-> (vlad/validate validation %)
-                               (vlad/assign-names field-names)
-                               (vlad/translate-errors vlad/english-translation))
-              :path               :form
-              :prevent-default?   true
-              :clean-on-unmount?  true
-              :props       {:is-open? (rf/subscribe
-                                       [:delete-open?])}
-              ;; :on-submit-response {400 "client error"
-              ;;                      500 "server error"}
-              :on-submit #(rf/dispatch [::delete-submit-handler %])}
-   (partial delete-dialog-form props)])
-
-(defn add-dialog-form [{:keys [^js classes]}
-                       {:keys [values
-                               props
-                               state
-                               form-id
-                               dirty
-                               errors
-                               set-values
-                               handle-change
-                               handle-blur
-                               handle-submit
-                               reset
-                               submitting?
-                               touched
-                               on-submit-response]}]
-  (let [{:keys [is-open?]} props]
-    [:form
-     {:id        form-id
-      :on-submit handle-submit}
-     [:> mui/Dialog {:fullWidth       true
-                     :open            @is-open?
-                     :on-close        #(rf/dispatch
-                                        [::close-add-dialog])
-                     :aria-labelledby :add-location-title}
-      [:> mui/DialogTitle {:id :add-location-title} "Add Location"]
-      [:> mui/DialogContent
-       [:> mui/DialogContentText "Enter a Sharepoint Name and URL "
-        " to schedule search scans."]
-       #_[:div [:pre (with-out-str
-                       (cljs.pprint/pprint touched)
-                       (cljs.pprint/pprint values)
-                       (cljs.pprint/pprint (or errors
-                                               submitting?)))]]
-       [:> TextField {:auto-focus true
-                      :required   true
-                      :full-width true
-                      :margin     "dense"
-                      :name       "name"
-                      :label      "Name"
-                      :type       "text"
-                      :helperText (when (touched "name")
-                                    (first (get errors (list "name"))))
-                      :error      (and (touched "name")
-                                       (first (get errors (list "name"))))
-                      :value      (values "name")
-                      :on-blur    handle-blur
-                      :on-change  handle-change}]
-       [:> TextField {:required   true
-                      :full-width true
-                      :helperText (when (touched "url")
-                                    (first (get errors (list "url"))))
-                      :error      (and (touched "url")
-                                       (first (get errors (list "url"))))
-                      :value      (values "url")
-                      :on-blur    handle-blur
-                      :on-change  handle-change
-                      :margin     "dense"
-                      :name       "url"
-                      :label      "URL"
-                      :type       "url"}]
-       [:> FormControl {:class-name  (.-formControl classes)
-                        :margin     "dense"
-                        :full-width true
-                        :label      "Policy"}
-        [:> InputLabel {:id "policy-selecter-label"} "Policy"]
-        [:> Select {:label-id  "policy-selecter-label"
-                    :id        "policy"
-                    :name      "policy"
-                    :value     (values "policy")
-                    :on-blur   handle-blur
-                    ;;   material ui does something funky with the
-                    ;;   event target so a custom handle-change is
-                    ;;   required
-                    :on-change #(swap! state
-                                       assoc-in [:values "policy"]
-                                       (-> % .-target .-value))
-                    }
-         [:> MenuItem {:name "none"
-                       :value nil} [:em "None"]]
-         [:> MenuItem {:name "low"
-                       :value "Low"} "Low"]
-         [:> MenuItem {:name "high"
-                       :value "High"} "High"]]]]
-      [:> DialogActions
-       [:> Button {:label    "Cancel"
-                   :on-blur   handle-blur
-                   :color    "primary"
-                   :on-click (fn [e]
-                               (reset)
-                               (rf/dispatch [:cancel-add]))}
-        "Cancel"]
-       [:> Button {:type     "submit"
-                   :on-blur   handle-blur
-                   :class    (.-submit classes)
-                   :color    "primary"
-                   :disabled (or errors submitting?)
-                   :on-click handle-submit
-                   :variant  "contained"
-                   :label    "Save Location"} "Save"]]]]))
-
-(defn add-dialog [props]
-  [fork/form {:form-id         "id"
-              :validation #(-> (vlad/validate validation %)
-                               (vlad/assign-names field-names)
-                               (vlad/translate-errors vlad/english-translation))
-              :path               :form
-              :prevent-default?   true
-              :clean-on-unmount?  true
-              :props       {:is-open? (rf/subscribe
-                                       [:add-open?])}
-              ;; :on-submit-response {400 "client error"
-              ;;                      500 "server error"}
-              :on-submit #(rf/dispatch [::add-submit-handler %])}
-   (partial add-dialog-form props)])
 
 (defn edit-dialog-form [{:keys [^js classes]}
                         {:keys [values
@@ -622,18 +260,7 @@
                              :variant       "h6"
                              :color         "primary"
                              :gutter-bottom true}
-          "Scan Locations"]]
-        [:> Grid {:item true}
-         [:> Tooltip {:title "Add Location"}
-          [:> Button {:classes    (.-button classes)
-                      :style      {:float "right"}
-                      :color      "primary"
-                      :aria-label "Add Location"
-                      :on-click   #(rf/dispatch [::open-add-dialog %])
-                      :startIcon  (reagent/create-element icons/AddLocation)}
-
-           "Add"]]
-         ]]
+          "Scan Locations"]]]
        [:> mui/Table {:size "small"}
         [:> mui/TableHead
          [:> mui/TableRow {:class (.-hoverable classes)}
@@ -674,28 +301,12 @@
                                  :class      (.-avatar classes)
                                  :on-click   #(rf/dispatch
                                                [::edit-location loc])}
-                  [:> icons/Edit]]]
-                [:> Tooltip {:title (str "Delete " name)}
-                 [::> IconButton {:aria-label "delete"
-                                  :color      "primary"
-                                  :class      (.-avatar classes)
-                                  :on-click   #(rf/dispatch
-                                                [::delete-location loc])}
-                  [:> icons/Delete]]]
-                ]
-               ]]))]] ])))
+                  [:> icons/Edit]]]] ]]))]] ])))
 
-
-(defn location-table-widget [kv]
-  [:> (with-styles
-        (reagent/reactify-component
-         (location-table kv)))])
 
 (defn main [{:keys [^js classes]}]
   [:<>
-   [add-dialog {:classes classes}]
    [edit-dialog {:classes classes}]
-   [delete-dialog {:classes classes}]
 
    [:> Container {:max-width "lg" :class (.-container classes)}
     [:> Grid {:container true :spacing 3}
@@ -703,8 +314,6 @@
       [:> Paper {:max-width "lg"
                  :spacing 2
                  :class (.-paper classes)}
-       #_[hover-sample {:classes classes}]
-       #_[add-location-button-widget {:classes classes}]
        [location-table {:classes classes}]
        ]]
      [:> Grid {:container true :spacing 3}]]]])
